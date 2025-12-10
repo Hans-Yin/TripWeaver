1798fa6734818613619702177fe2c4b266c28a3f
Hans-Yin <tftzseckill@gmail.com>
2025-12-10 18:01:10 -0500

    Deploy Online

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 // src/api.ts
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://tripweaver.onrender.com";
 
 export interface TripRequest {
   query: string;
